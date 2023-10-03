d0c303236e3a0120df2e482487a33b4c46da69b9
nsanzimfura-eric <erickykress1@gmail.com>
2023-10-03 23:45:51 +0200

    get images and change to docker ps

--- server/controllers/generatePdf.controller.js
+++ server/controllers/generatePdf.controller.js
@@ -1,47 +1,17 @@
-const { hostname } = require("../constants/constants");
-const { executablePath } = require("puppeteer");
+const convertWebsiteToPDF = require("../services/convertToPdf.service");
 
-const puppeteer = require("puppeteer-extra");
-const hidden = require("puppeteer-extra-plugin-stealth");
-
-const generatePdfControler = (_, res) => {
-  (async () => {
-    let browser;
-    try {
-      puppeteer.use(hidden());
-      browser = await puppeteer.launch({
-        args: ["--no-sandbox"],
-        headless: false,
-        ignoreHTTPSErrors: true,
-        executablePath: executablePath(),
-      });
-
-      const page = await browser.newPage();
-      await page.goto(hostname, { waitUntil: "networkidle0" });
-      await page.setViewport({
-        width: 1920,
-        height: 1080,
-        deviceScaleFactor: 1,
-      });
-      await page.waitForSelector("img");
-      await page.waitForTimeout(3000);
-
-      const pdfBuffer = await page.pdf({ format: "A4" });
-      res.setHeader("Content-Type", "application/pdf");
-      res.setHeader(
-        "Content-Disposition",
-        "attachment; filename=market-insights.pdf"
-      );
-      res.send(pdfBuffer);
-    } catch (error) {
-      console.error("Error generating PDF:", error);
-      res.status(500).send("Error generating PDF");
-    } finally {
-      if (browser) {
-        await browser.close();
-      }
-    }
-  })();
+const generatePdfControler = async (req, res) => {
+  try {
+    const pdf = await convertWebsiteToPDF();
+    res.set({
+      "Content-Type": "application/pdf",
+      "Content-Disposition": `attachment; filename=market_insights${new Date().getTime()}.pdf`,
+    });
+    res.send(pdf);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Error generating PDF.");
+  }
 };
 
 module.exports = generatePdfControler;
