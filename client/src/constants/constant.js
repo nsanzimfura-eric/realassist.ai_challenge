650d973b70876c15839cf1ab1910af410501bd4f
nsanzimfura-eric <erickykress1@gmail.com>
2023-10-03 14:34:29 +0200

    user server as hosted

--- client/src/constants/constant.js
+++ client/src/constants/constant.js
@@ -1,3 +1,3 @@
 export const server_url =
-  //   "https://pdf-server-self.vercel.app/actions/generate-pdf";
-  "http://localhost:3001/actions/generate-pdf";
+  "https://pdf-server-self.vercel.app/actions/generate-pdf";
+// ("http://localhost:3001/actions/generate-pdf");
