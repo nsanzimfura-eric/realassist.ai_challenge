c442ba22de6bd2bd9449bb7678fc4645cec18950
nsanzimfura-eric <erickykress1@gmail.com>
2023-10-03 19:23:08 +0200

    uncomment puppeteer

--- server/controllers/generatePdf.controller.js
+++ server/controllers/generatePdf.controller.js
@@ -1,4 +1,4 @@
-// const puppeteer = require("puppeteer");
+const puppeteer = require("puppeteer");
 const { hostname } = require("../constants/constants");
 
 const generatePdfControler = (_, res) => {
