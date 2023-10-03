8f1a6604710bffe60f11e5543d39e97cddde92b5
nsanzimfura-eric <erickykress1@gmail.com>
2023-10-03 13:15:35 +0200

    use the IIFE structure and explicitly call close() to ensure the browser is closed in the IIFE scope

--- server/controllers/generatePdf.controller.js
+++ server/controllers/generatePdf.controller.js
@@ -1,25 +1,29 @@
 const puppeteer = require("puppeteer");
 const { hostname } = require("../constants/constants");
 
-const generatePdfControler = async (req, res) => {
-  const browser = await puppeteer.launch({ headless: true });
-  const page = await browser.newPage();
-
-  try {
-    await page.goto(hostname);
-    const pdfBuffer = await page.pdf({ format: "A4" });
-    await browser.close();
-    res.setHeader("Content-Type", "application/pdf");
-    res.setHeader(
-      "Content-Disposition",
-      "attachment; filename=market-insights.pdf"
-    );
-
-    res.send(pdfBuffer);
-  } catch (error) {
-    console.error("Error generating PDF:", error);
-    res.status(500).send("Error generating PDF");
-  }
+const generatePdfControler = (_, res) => {
+  (async () => {
+    let browser;
+    try {
+      browser = await puppeteer.launch({ headless: true });
+      const page = await browser.newPage();
+      await page.goto(hostname);
+      const pdfBuffer = await page.pdf({ format: "A4" });
+      res.setHeader("Content-Type", "application/pdf");
+      res.setHeader(
+        "Content-Disposition",
+        "attachment; filename=market-insights.pdf"
+      );
+      res.send(pdfBuffer);
+    } catch (error) {
+      console.error("Error generating PDF:", error);
+      res.status(500).send("Error generating PDF");
+    } finally {
+      if (browser) {
+        await browser.close();
+      }
+    }
+  })();
 };
 
 module.exports = generatePdfControler;
