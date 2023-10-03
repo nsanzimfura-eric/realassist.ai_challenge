6057a9c7948d79b816bc2b036ed5fa7259193667
nsanzimfura-eric <erickykress1@gmail.com>
2023-10-03 13:03:13 +0200

    solve issue with unhandeled rejection

--- server/controllers/generatePdf.controller.js
+++ server/controllers/generatePdf.controller.js
@@ -4,9 +4,9 @@ const { hostname } = require("../constants/constants");
 const generatePdfControler = async (req, res) => {
   const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
-  await page.goto(hostname).promise();
 
   try {
+    await page.goto(hostname);
     const pdfBuffer = await page.pdf({ format: "A4" });
     await browser.close();
     res.setHeader("Content-Type", "application/pdf");
