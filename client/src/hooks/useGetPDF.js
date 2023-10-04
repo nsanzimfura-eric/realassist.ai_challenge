941e085a0d3c4d76816c2d9a134386a2e0ea4423
nsanzimfura-eric <erickykress1@gmail.com>
2023-10-05 00:32:50 +0200

    remove dom

--- client/src/hooks/useGetPDF.js
+++ client/src/hooks/useGetPDF.js
@@ -1,18 +1,12 @@
 import { useState } from "react";
 import axios from "axios";
 import { baseURI, server_url } from "../constants/constant";
-import { useNavigate } from "react-router-dom";
-import { ROUTES } from "../utils/routes";
-
-const uiUrlToCovert = `${baseURI}/${ROUTES.download}`;
 
 export const useGetDataPdf = () => {
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
-  const nagitate = useNavigate();
 
   const handleDownload = async () => {
-    nagitate(ROUTES.download);
     try {
       setLoading(true);
       const response = await axios.get(`${server_url}?url=${baseURI}`, {
@@ -27,7 +21,6 @@ export const useGetDataPdf = () => {
       a.click();
       window.URL.revokeObjectURL(url);
       setLoading(false);
-      nagitate(ROUTES.home);
     } catch (error) {
       setError(error);
       setLoading(false);
