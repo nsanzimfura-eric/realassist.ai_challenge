2f2d50c1dfa077b5af676c7859bd5c6d5b4980b2
nsanzimfura-eric <erickykress1@gmail.com>
2023-10-04 17:31:28 +0200

    change server and remove loading icons

--- client/src/constants/constant.js
+++ client/src/constants/constant.js
@@ -1,4 +1,4 @@
-// export const server_url = "https://server-ai.onrender.com/actions/generate-pdf";
-export const server_url = "http://localhost:3001/actions/generate-pdf";
+export const server_url = "https://server-ai.onrender.com/actions/generate-pdf";
+// export const server_url = "http://localhost:3001/actions/generate-pdf";
 export const baseAPI_route =
   "https://api.usa.gov/crime/fbi/cde/arrest/state/AK/all?from=2015&to=2020&API_KEY=iiHnOKfno2Mgkt5AynpvPpUQTEyxE77jo1RU8PIv";
